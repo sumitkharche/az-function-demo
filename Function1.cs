7f4e25d9bb887372a1d92a2f6334eb6a69a6ab09
shweta Bonde <shwetavb.18@gmail.com>
2024-04-16 16:28:29 +0530

    first commit

---
+++ Function1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp5
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public async Task Run(
+            [ServiceBusTrigger("reporting", Connection = "ServiceBusConnection")]
+            ServiceBusReceivedMessage message,
+            ServiceBusMessageActions messageActions)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+            // Write Code to Call the power BI API here
+
+
+            // Complete the message
+            await messageActions.CompleteMessageAsync(message);
+        }
+    }
+}
